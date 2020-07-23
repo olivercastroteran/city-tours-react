1c13002f65ae797a4c2cd6c01bf00115e9c440df
olivercastroteran <olivercastroteran@gmail.com>
2020-07-23 08:42:16 -0400

    showing tour info on click event

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './Tour.scss';
 
 class Tour extends Component {
+  state = {
+    showInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({ showInfo: !this.state.showInfo });
+  };
+
   render() {
     const { city, img, name, info } = this.props.tour;
     return (
@@ -17,11 +25,11 @@ class Tour extends Component {
           <h4>{name}</h4>
           <h5>
             info{' '}
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down"></i>
             </span>
           </h5>
-          <p>{info}</p>
+          {this.state.showInfo ? <p>{info}</p> : null}
         </div>
       </article>
     );
